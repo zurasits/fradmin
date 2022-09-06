71efdce2acd13bdf15bce5f7296773a608c9abbc
zurasits <zurasits@gmail.com>
2022-09-07 03:40:16 +0400

    orders data added, dynamic order show. project done

---
+++ orders.js
@@ -0,0 +1,40 @@
+const Orders = [
+{
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'LAVRENDER KF102 Drohne',
+    productNumber: '36378',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName: 'Ruko F11 Pro Drone',
+    productNumber: '29165',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Drone with Camera Drone',
+    productNumber: '19385',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'GPS 4k Drone',
+    productNumber: '31298',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'Lozenge Drone',
+    productNumber: '00154',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+}
+
+
+];
